c92beadcbb69072834a7ba963aee749b7cf6e66a
AriadnaGol <ariadna.golebicka@gmail.com>
2022-09-16 00:15:30 +0200

    update project

--- src/main/java/pl/cyber/trainess/demo/endpoint/PierwszyController.java
+++ src/main/java/pl/cyber/trainess/demo/endpoint/PierwszyController.java
@@ -1,18 +1,25 @@
 package pl.cyber.trainess.demo.endpoint;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.cyber.trainess.demo.dto.ImieDTO;
+import pl.cyber.trainess.demo.service.PersonService;
 
 /**
  * @author Mariusz Tański
  */
 
+@Slf4j //odpowiada za logi w ramach serwera
 @RestController //Adnotacja mówi serwera Spring, że w tym miejscu będą funkcjonalności REST API
+@RequiredArgsConstructor
 @RequestMapping("/v1/first")
 public class PierwszyController {
+
+  private final PersonService service;
 
   //HTTP metoda GET - metoda służy do pobierania informacji z serwera
   @GetMapping("/{imie}")
@@ -21,4 +28,5 @@ public class PierwszyController {
         .imie(imie)
         .build();
   }
+
 }
